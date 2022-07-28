10f898516f15bda39a6de48248838d2e4c15a0f4
Nicolas Agudelo <nicoalbert95@hotmail.com>
2022-07-28 15:06:55 -0400

    Refactored the swap logic into the swap function, optimized code to make less comparisons by not taking into account the numbers that already were sorted

--- Bubble/bubble.py
+++ Bubble/bubble.py
@@ -1,27 +1,20 @@
-from re import A
-
-
 def bubble_sort(lst, ascending = True):
-    # print(lst)
-    unordered = True
-    while unordered:
-        unordered = False
-        for index in range(len(lst) - 1):
+    for i in range(len(lst)):
+        for index in range(len(lst) -i - 1):
             if lst[index] > lst[index + 1] and ascending == True:
-                temp = lst[index]
-                lst[index] = lst[index + 1]
-                lst[index + 1 ] = temp
-                unordered = True
+                swap(lst, index, index + 1)
             if lst[index] < lst[index + 1] and ascending == False:
-                temp = lst[index]
-                lst[index] = lst[index + 1]
-                lst[index + 1 ] = temp
-                unordered = True
-        # print(lst)
+                swap(lst, index, index + 1)
 
     return lst
+
+def swap(lst, i1, i2):
+    temp = lst[i1]
+    lst[i1] = lst[i2]
+    lst[i2] = temp
 
 print(bubble_sort([5,2,9,1,5]))
 print(bubble_sort([5,2,9,1,5], False))
 print(bubble_sort([3,5,4,6,2,1]))
 print(bubble_sort([3,5,4,6,2,1], False))
+print(bubble_sort([2,6,3,5,1]))
